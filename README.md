bf2edf99360613d4579db94c1ee858baab8f8eb9
Shalom91 <shalom.mothapo@gmail.com>
2021-01-26 19:25:12 +0200

    updated readme file

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# S&P 500 APP
-Dispays S&P 500 data and makes it available for dowload
+![Cover Image](logo.png)
+# NASDAQ 100 APP
+Displays NASDAQ 100 data and makes it available for download.
+
+## How to run the app
+- Type `streamlit run myapp.py` in your terminal to launch the web app in your browser.
+- Use the sidebar multiselector to filter data by sector.
+- Click on the `Download CSV File` link to download selected data.
+- Use the slide to choose the number of plots to be shown.
+- Click the `Show Plots` button to show the plots.
--- app.py
+++ app.py
@@ -3,22 +3,26 @@ import pandas as pd
 import base64
 import matplotlib.pyplot as plt
 import yfinance as yf
+from PIL import Image
 
-st.title("S&P 500 App")
+image = Image.open('logo.png')
+st.image(image, use_column_width=True)
+
+st.title("NASDAQ-100 App")
 
 st.markdown("""
-This app displays **S&P 500** data
+This app displays **NASDAQ 100** data
 The data can also be exported as a csv file
 """)
 
 st.sidebar.header("User Input Features")
 
-# Web scrapping S&P data
+# Web scrapping NASDAQ data
 @st.cache
 def load_data():
-    url = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
+    url = 'https://en.wikipedia.org/wiki/NASDAQ-100'
     html = pd.read_html(url, header=0)
-    df = html[0]
+    df = html[3]
     return df
 
 df = load_data()
@@ -26,11 +30,55 @@ sector = df.groupby('GICS Sector')
 
 # Sidebar: Sector selection
 sorted_sector = sorted(df['GICS Sector'].unique())
-selected_sector = st.sidebar.multiselect("Sector", sorted_sector)
+selected_sector = st.sidebar.multiselect("Sector", sorted_sector, sorted_sector)
 
 # Filter Data
 df_selected_sector = df[(df['GICS Sector'].isin(selected_sector))]
 
 st.header("Display Companies in Selected Sector")
-st.write("Data Dimension: " + str(df_selected_sector.shape[0]) + " and " + str(df_selected_sector.shape[1]) + " columns")
+st.write("Data Dimension: " + str(df_selected_sector.shape[0]) + " rows and " + str(df_selected_sector.shape[1]) + " columns")
 st.dataframe(df_selected_sector)
+
+# Donwnload SP500 data
+def filedownload(df):
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()
+    href = f'<a href="data:file/csv;base64,{b64}" download="NASDAQ100.csv">Download CSV File</a>'
+    return href
+
+st.markdown(filedownload(df_selected_sector), unsafe_allow_html=True)
+
+data = yf.download(
+    tickers=list(df_selected_sector[:10].Ticker),
+    start = "2020-01-01",
+    end = "2020-12-30",
+    interval = "1d",
+    group_by = 'ticker',
+    auto_adjust = True,
+    prepost = True,
+    threads = True,
+    proxy = None
+)
+
+# Plot closing price
+def price_plot(symbol):
+    df = pd.DataFrame(data[symbol].Close)
+    df['Date'] = df.index
+    plt.fill_between(df.Date, df.Close, color='green', alpha=0.3)
+    plt.plot(df.Date, df.Close, color='skyblue', alpha=0.8)
+    plt.xticks(rotation=90)
+    plt.title(symbol, fontweight='bold')
+    plt.xlabel('Date', fontweight='bold')
+    plt.ylabel('Closing Price', fontweight='bold')
+    st.set_option('deprecation.showPyplotGlobalUse', False)
+    return st.pyplot()
+
+number_slider = st.sidebar.slider("Number of Companies", 1, 5)
+
+if st.button("Show Plots"):
+    st.header("Closing Price")
+    for i in list(df_selected_sector.Ticker)[:number_slider]:
+        price_plot(i)
+
+
+    
